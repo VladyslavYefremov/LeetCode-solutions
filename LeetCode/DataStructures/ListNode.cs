e53d569c0b4a98cc39eb946deb09eac5da5e5896
Vladyslav Yefremov <Vladyslav.Yefremov@infopulse.com>
2018-03-12 15:40:52 +0200

    Implement Add Two Numbers solution

---
+++ LeetCode/DataStructures/ListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.DataStructures
+{
+	/// <summary>
+	/// Definition for singly-linked list
+	/// 
+	/// ref: https://leetcode.com/problems/add-two-numbers/description/
+	/// </summary>
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int x) { val = x; }
+	}
+}
---
+++ LeetCode/Solutions/Medium/AddTwoNumbersSolution.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.DataStructures;
+using LeetCode.Tasks.Medium;
+
+namespace LeetCode.Solutions.Medium
+{
+	public class AddTwoNumbersSolution : IAddTwoNumbers
+	{
+		public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+		{
+			var current1 = l1;
+			var current2 = l2;
+
+			ListNode resultHead = null;
+			ListNode currentResultNode = null;
+
+			while (current1 != null || current2 != null)
+			{
+				if (resultHead == null)
+				{
+					resultHead = new ListNode(0);
+				}
+
+				if (currentResultNode == null)
+				{
+					currentResultNode = resultHead;
+				}
+				else
+				{
+					if (currentResultNode.next == null) currentResultNode.next = new ListNode(0);
+
+					currentResultNode = currentResultNode.next;
+				}
+
+				if (current1 != null) currentResultNode.val += current1.val;
+				if (current2 != null) currentResultNode.val += current2.val;
+
+				current1 = current1?.next;
+				current2 = current2?.next;
+
+				if (!(currentResultNode.val < 10))
+				{
+					currentResultNode.next = new ListNode(currentResultNode.val / 10);
+					currentResultNode.val %= 10;
+				}
+			}
+
+			return resultHead;
+		}
+	}
+}
