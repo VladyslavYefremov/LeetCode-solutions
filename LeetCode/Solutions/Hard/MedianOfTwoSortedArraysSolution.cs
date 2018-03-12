14352e497dd2d326034a3f23eeee121023570af1
Vladyslav Yefremov <Vladyslav.Yefremov@infopulse.com>
2018-03-12 18:25:48 +0200

    Add solution of Median of Two Sorted Arrays task with extra space usage

---
+++ LeetCode/Solutions/Hard/MedianOfTwoSortedArraysSolution.cs
@@ -0,0 +1,57 @@
+﻿using LeetCode.Tasks.Hard;
+
+namespace LeetCode.Solutions.Hard
+{
+	/// <summary>
+	/// Find median of two sorted arrays by using extra space
+	/// </summary>
+	public class MedianOfTwoSortedArraysSolution : IMedianOfTwoSortedArrays
+	{
+		public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+		{
+			var l1 = nums1.Length;
+			var l2 = nums2.Length;
+			var n = l1 + l2;
+
+			var aggregator = new int[n];
+
+			int ai1 = 0, // element index of the first array
+				ai2 = 0, // element index of the second array
+				aia = 0; // element index of the aggregator array
+
+			while (ai1 < l1 || ai2 < l2)
+			{
+				if (ai1 < l1)
+				{
+					if (ai2 < l2)
+					{
+						if (nums1[ai1] < nums2[ai2])
+						{
+							aggregator[aia++] = nums1[ai1];
+							ai1++;
+						}
+						else
+						{
+							aggregator[aia++] = nums2[ai2];
+							ai2++;
+						}
+					}
+					else
+					{
+						aggregator[aia++] = nums1[ai1];
+						ai1++;
+					}
+				}
+				else if (ai2 < l2)
+				{
+					aggregator[aia++] = nums2[ai2];
+					ai2++;
+				}
+			}
+
+			var medianIndex = (n - 1) / 2;
+
+			return n % 2 != 0 ? aggregator[medianIndex] : (aggregator[medianIndex] + aggregator[medianIndex + 1]) / 2.0;
+		}
+	}
+}
---
+++ LeetCode/Tasks/Hard/IMedianOfTwoSortedArrays.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Tasks.Hard
+{
+	/// <summary>
+	/// There are two sorted arrays nums1 and nums2 of size m and n respectively.
+	/// 
+	/// Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+	/// 
+	///	Example 1:
+	///		nums1 = [1, 3]
+	///		nums2 = [2]
+	///		
+	///		The median is 2.0
+	/// 
+	///	Example 2:
+	///		nums1 = [1, 2]
+	///		nums2 = [3, 4]
+	/// 
+	///		The median is (2 + 3)/2 = 2.5
+	/// 
+	/// ref: https://leetcode.com/problems/median-of-two-sorted-arrays/
+	/// </summary>
+	public interface IMedianOfTwoSortedArrays
+	{
+		double FindMedianSortedArrays(int[] nums1, int[] nums2);
+	}
+}
