1dbd860d8925f06199f5b5315c6eac5b1bbbb027
Vladyslav Yefremov <Vladyslav.Yefremov@infopulse.com>
2018-04-24 14:28:04 +0300

    Refactor MergeTwoSortedLists solution

--- LeetCode/Solutions/Easy/MergeTwoSortedLists.cs
+++ LeetCode/Solutions/Easy/MergeTwoSortedLists.cs
@@ -13,20 +13,21 @@ namespace Leetcode.Solutions.Easy
 			while (l1 != null || l2 != null)
 			{
 				var smallest = l1 == null ? l2 : (l2 == null ? l1 : (l1.val < l2.val ? l1 : l2));
-
-				if (headNode == null)
-				{
-					headNode = new ListNode(smallest.val);
-					currentNode = headNode;
-				}
-				else
-				{
-					currentNode.next = new ListNode(smallest.val);
-					currentNode = currentNode.next;
-				}
+				var smallestNode = new ListNode(smallest.val);
 
 				if (l1 == smallest) l1 = l1.next;
 				else if (l2 == smallest) l2 = l2.next;
+
+				if (headNode == null)
+				{
+					headNode = smallestNode;
+					currentNode = headNode;
+
+					continue;
+				}
+
+				currentNode.next = smallestNode;
+				currentNode = currentNode.next;
 			}
 
 			return headNode;
