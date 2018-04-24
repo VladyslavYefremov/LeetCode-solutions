6666f65ebc4ff2cb0f3accdcc317e0001a721a00
Vladyslav Yefremov <Vladyslav.Yefremov@infopulse.com>
2018-04-24 14:27:45 +0300

    Add solution of Container With Most Water task

---
+++ LeetCode/Solutions/Medium/ContainerWithMostWater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LeetCode.Tasks.Medium;
+
+namespace LeetCode.Solutions.Medium
+{
+	public class ContainerWithMostWater : IContainerWithMostWater
+	{
+		public int MaxArea(int[] height)
+		{
+			var n = height.Length;
+			var maxArea = 0;
+			var leftIndex = 0;
+			var rightIndex = n - 1;
+
+			while (leftIndex != rightIndex)
+			{
+				var leftValue = height[leftIndex];
+				var rightValue = height[rightIndex];
+				var minValue = Math.Min(leftValue, rightValue);
+				var distance = Math.Abs(rightIndex - leftIndex);
+
+				var area = minValue * distance;
+
+				if (area > maxArea) maxArea = area;
+
+				if (minValue == leftValue) leftIndex++;
+				else if (minValue == rightValue) rightIndex--;
+			}
+
+			return maxArea;
+		}
+	}
+}
